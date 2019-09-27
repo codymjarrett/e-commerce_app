0336cffd51be5597c3cbc28327fe80db54839636
Cody Jarrett <cody.jarrett@americaneagle.com>
2019-09-27 15:16:09 -0500

    updated our menu items to navigate withRouter HOC

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
 
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = props => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>Hats p[age</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div >
-     <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
